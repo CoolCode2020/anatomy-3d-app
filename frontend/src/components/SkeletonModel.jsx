e1e6e1ed853f24d18a4dce131c398d7c7bb1e1af
Horni <davidhorn93@hotmail.com>
2025-04-30 16:08:18 +0200

    Skelet in backend auf website gerendet

--- frontend/src/components/SkeletonModel.jsx
+++ frontend/src/components/SkeletonModel.jsx
@@ -1,6 +1,6 @@
 import { useGLTF } from '@react-three/drei'
 
 export function SkeletonModel() {
-  const { scene } = useGLTF('/Skelet.glb')  // Public-Ordner wird direkt referenziert von vite
+  const { scene } = useGLTF('http://localhost:8080/models/Skelet.glb')  // Public-Ordner wird direkt referenziert von vite
   return <primitive object={scene} />
 }
