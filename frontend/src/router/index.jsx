3ee6acd3f3e77bc19234bc374bdf7a08340b8eae
Horni <davidhorn93@hotmail.com>
2026-04-28 13:46:48 +0200

    renamed HomePage to Homepage

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
-import HomePage from '../pages/HomePage';
+import Homepage from '../pages/Homepage';
 import Quiz1 from '../pages/Quiz1';     // Quiz 1
 import Quiz2 from '../pages/Quiz2'; // Quiz 2
 import InfoPage from '../pages/Infopage';   // Info
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
         path: '/',
         element: <App />, // Layout mit Navbar und Outlet
         children: [
-            { path: '', element: <HomePage /> },
+            { path: '', element: <Homepage /> },
             { path: 'quiz1', element: <Quiz1 /> },
             { path: 'quiz2', element: <Quiz2 /> },
             { path: 'info', element: <InfoPage /> },
